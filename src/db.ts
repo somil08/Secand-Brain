38bd9d700a6301ace0722e75ea519765928b4c93
somil08 <Somilrathore0208@gmail.com>
2025-02-12 12:38:09 +0530

    v1-progress

--- src/db.ts
+++ src/db.ts
@@ -1,1 +1,27 @@
-console.log("ubaua")
+
+import mongoose, {model, Schema} from "mongoose";
+
+mongoose.connect("mongodb://localhost:27017/brainly")
+
+const UserSchema = new Schema({
+    username: {type: String, unique: true},
+    password: String
+})
+
+export const UserModel = model("User", UserSchema);
+
+const ContentSchema = new Schema({
+    title: String,
+    link: String,
+    tags: [{type: mongoose.Types.ObjectId, ref: 'Tag'}],
+    type: String,
+    userId: {type: mongoose.Types.ObjectId, ref: 'User', required: true },
+})
+
+const LinkSchema = new Schema({
+    hash: String,
+    userId: {type: mongoose.Types.ObjectId, ref: 'User', required: true, unique: true },
+})
+
+export const LinkModel = model("Links", LinkSchema);
+export const ContentModel = model("Content", ContentSchema);
